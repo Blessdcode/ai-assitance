5f63ef8822089ee46cf8c3c10c010bdf9c5a9efc
Blessdbnjmn <113176355+Blessdcode@users.noreply.github.com>
2025-03-18 12:40:54 +0100

    basic set-up made including database and others

---
+++ components/constants/navigation.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navigation = () => {
+  return (
+    <div className='boxWidth boxMar'>
+      <Link href="/">PDF Companion</Link>
+    </div>
+  )
+}
+
+export default Navigation
